70c276434ab7939166e6c412af3149acb1775437
Alexey <Alexey261139@mail.ru>
2024-07-09 22:57:53 +0300

    Добавьте файлы проекта.

---
+++ Tasks/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Tasks;
+
+public class Program
+{
+    private static Task1 task1 = new();
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            var inputNumber = int.Parse(Console.ReadLine() ?? "");
+            int sum = task1.GetOneDigitSum(inputNumber);
+        }
+    }
+}
+
+public class Task1
+{
+    public int GetOneDigitSum(int number)
+    {
+        int sum = SumOfTheDigitsInTheNumber(number);
+        Console.WriteLine(sum);
+
+        return (sum / 10 == 0)
+            ? sum
+            : GetOneDigitSum(sum);
+    }
+
+    public int SumOfTheDigitsInTheNumber(int number)
+        => number == 0
+        ? 0
+        : number % 10 + SumOfTheDigitsInTheNumber(number / 10);
+}
